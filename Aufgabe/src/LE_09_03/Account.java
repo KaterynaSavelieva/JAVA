5d17f7f4fa456c9953f694b00797ce4371158ea2
KaterynaSaveleiva <katrunyzya@gmail.com>
2025-12-22 09:42:55 +0100

    +

---
+++ Aufgabe/src/LE_09_03/Account.java
@@ -0,0 +1,66 @@
+package LE_09_03;
+
+public class Account {
+    private final String accountNumber;
+    private final String owner;
+    private double balance;
+
+    public Account(String accountNumber, String owner, double startBalance) {
+        if (accountNumber == null || accountNumber.isBlank()) { // isBlank() checks whether a string is empty or contains only whitespace characters.
+            throw new IllegalArgumentException("Account number must not be empty.");
+        }
+        if (owner == null || owner.isBlank()) {
+            throw new IllegalArgumentException("Owner must not be empty.");
+        }
+        if (startBalance < 0) {
+            throw new IllegalArgumentException("\"Start balance cannot be negative.");
+        }
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = startBalance;
+    }
+
+
+    // ===== Business methods =====
+    public void deposit(double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Deposit amount must be greater than 0.");
+        }
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        /* Логіка:
+            неправильні дані → exception
+            недостатньо грошей → false
+            успіх → true
+         */
+        if (amount < 0) {
+            throw new IllegalArgumentException("Withdraw amount must be greater than 0.");
+        }
+        if (amount > balance) {
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+
+
+    // ===== Getters =====
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Account number: " + accountNumber + ", owner: " + owner + ", balance: " + balance;
+    }
+}
---
+++ Aufgabe/src/LE_09_03/App.java
@@ -0,0 +1,27 @@
+package LE_09_03;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        AccountManagement manager = new AccountManagement();
+
+
+        private static void printMenu() {
+            System.out.println("=== Account Management ===");
+            System.out.println("1) Create account");
+            System.out.println("2) Delete account");
+            System.out.println("3) Deposit money");
+            System.out.println("4) Withdraw money");
+            System.out.println("5) Show balance");
+            System.out.println("6) Show one account");
+            System.out.println("7) Show all accounts");
+            System.out.println("0) Exit");
+        }
+
+
+    }
+}
